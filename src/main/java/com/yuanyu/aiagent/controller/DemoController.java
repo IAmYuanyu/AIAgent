c46e72d81737f5d254569c10517ec8bf27bd288a
缘鱼 <15557773+iamyuanyu@user.noreply.gitee.com>
2026-03-15 20:20:02 +0800

    测试ChatClient的注入方式

--- src/main/java/com/yuanyu/aiagent/controller/DemoController.java
+++ src/main/java/com/yuanyu/aiagent/controller/DemoController.java
@@ -1,13 +1,32 @@
 package com.yuanyu.aiagent.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    private final ChatClient chatClient;
+
+    public DemoController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/generation")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .system("用户说Ciallo时请回复‘柚子厨蒸鹅心’")
+                .user(userInput)
+                .call()
+                .content();
+    }
+
     @GetMapping("/check")
     public String check() {
         return "OK";
     }
+
+
 }
--- src/main/java/com/yuanyu/aiagent/demo/invoke/SpringAiAiInvoke.java
+++ src/main/java/com/yuanyu/aiagent/demo/invoke/SpringAiAiInvoke.java
@@ -1,6 +1,8 @@
 package com.yuanyu.aiagent.demo.invoke;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -16,13 +18,25 @@ public class SpringAiAiInvoke implements CommandLineRunner {
 
     private final ChatModel dashscopeChatModel;
 
+    // private final ChatClient chatClient;
 
     // 项目运行时自动调用
     @Override
     public void run(String... args) throws Exception {
-        AssistantMessage assistantMessage = dashscopeChatModel.call(new Prompt("Ciallo～(∠・ω< )⌒☆"))
-                .getResult() // 获取结果
-                .getOutput();// 获取输出
-        System.out.println(assistantMessage.getText());
+        // AssistantMessage assistantMessage = dashscopeChatModel.call(new Prompt("Ciallo～(∠・ω< )⌒☆"))
+        //         .getResult() // 获取结果
+        //         .getOutput();// 获取输出
+        // System.out.println(assistantMessage.getText());
+
+        ChatClient chatClient = ChatClient.builder(dashscopeChatModel)
+                .defaultSystem("用户说Ciallo时回复‘柚子厨蒸鹅心’")
+                .build();
+
+        String content = chatClient.prompt()
+                .user("Ciallo～(∠・ω< )⌒☆")
+                .call()
+                .content();
+
+        System.out.println(content);
     }
 }
