c7972e460f7821105fa66522c8729cb2acc17da4
缘鱼 <15557773+iamyuanyu@user.noreply.gitee.com>
2026-03-24 23:46:53 +0800

    加载Markdown文档时额外添加status信息

--- src/main/java/com/yuanyu/aiagent/rag/LoveAppDocumentLoader.java
+++ src/main/java/com/yuanyu/aiagent/rag/LoveAppDocumentLoader.java
@@ -29,11 +29,15 @@ public class LoveAppDocumentLoader {
         try {
             Resource[] resources = resourcePatternResolver.getResources("classpath:document/*.md");
             for (Resource resource : resources) {
+                String filename = resource.getFilename();
+                // 根据自己的文档名字来拆，比如3个恋爱文档都是 恋爱常见问题和回答 - xx篇.md 格式就能这么拆，但实际情况一般会复杂得多
+                String status = filename.substring(filename.length() - 6, filename.length() - 4);
                 MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.builder()
                         .withHorizontalRuleCreateDocument(true) // 遇到 Markdown 的水平分割线（例如 --- / ***）时，就把它当作“文档分隔符”来切分
                         .withIncludeCodeBlock(false) // 是否添加代码块
                         .withIncludeBlockquote(false) // 是否添加块引用
-                        .withAdditionalMetadata("filename", resource.getFilename()) // 添加元数据
+                        .withAdditionalMetadata("filename", filename) // 添加元数据
+                        .withAdditionalMetadata("status", status)
                         .build();
                 MarkdownDocumentReader reader = new MarkdownDocumentReader(resource, config);
                 allDocuments.addAll(reader.get());
