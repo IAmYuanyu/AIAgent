3427affd8c9f906de60b4d9600229667e3d5b3b1
缘鱼 <15557773+iamyuanyu@user.noreply.gitee.com>
2026-04-09 19:59:52 +0800

    实现图片搜索MCP服务开发

---
+++ yuanyu-image-search-mcp-server/src/test/java/com/yuanyu/yuanyuimagesearchmcpserver/YuanyuImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanyu.yuanyuimagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuanyuImageSearchMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
